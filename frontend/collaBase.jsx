ae4f54e28bd2a7de7273bf9774e49e0fc08c1b55
Kevin Huang <kevinht.huang@gmail.com>
2016-04-26 21:07:31 -0700

    changed for heroku test

---
+++ frontend/collaBase.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+document.AddEventListener('DOMContentLoaded', function(){
+  ReactDOM.render('<h1>Haikus are pretty</h1>', document.body);
+});
