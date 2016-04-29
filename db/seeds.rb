f37a86f39400fba76d02cfaeb4261425dc3dc27b
Kevin Huang <kevinht.huang@gmail.com>
2016-04-29 00:19:12 -0700

    added guest login seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!({email: "guest@guest.com", password: "password", organization: "House Stark", name: "Bob Stark"})
